2eb7d3f707d0b963cf7ab90c7e29580dca63bcef
supri <supri@LAPTOP-7H65MARM>
2022-02-12 00:41:11 +0530

    by supriya

--- GitHubDemo/src/com/cjc/git/Demo.java
+++ GitHubDemo/src/com/cjc/git/Demo.java
@@ -1,8 +1,21 @@
 package com.cjc.git;
 
 public class Demo {
+	public void m1()
+	{
+		System.out.println("Hello m1");
+	}
+	
+	public void m2()
+	{
+		System.out.println("hello m2");
+	}
+	
 public static void main(String[] args) {
 	
 	System.out.println("Hello Main of GitHub");
+	Demo d=new Demo();
+	d.m1();
+	d.m2();
 }
 }
